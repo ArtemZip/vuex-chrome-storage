285c1ccd01d6af8b92be32c1057191458a323c28
ArtemZip <abosnyak@ya.ru>
2023-10-27 00:11:50 +0200

    added vuex

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+export const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  },
+  getters: {
+    count (state) {
+        return state.count
+    }
+  }
+})
--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="main_app">
     <h1>Hello {{msg}}</h1>
+    <div>Count: {{count}} </div>
+    <div><button v-on:click="increment">increment</button></div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'popupView',
   data () {
     return {
       msg: 'popup'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'count'
+    ])
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
     }
   }
 }
@@ -24,5 +37,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 200px;
 }
 </style>
